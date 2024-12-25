aa7f617cc5a684e6a06ee41cb0cf9533aac3f072
Tinkal300 <tinkal300@gmail.com>
2024-12-25 18:39:38 +0530

    all setup config

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,21 @@
 import { asyncHandler } from "../utils/asyncHandler.js"
 
 const registerUser  = asyncHandler( async (req, res) =>{
- return res.status(200).json({
-    message: "ok"
-  })
+     
+       res.status(500).json({
+        message: "Tinkal Kumar"
+      })
+     
+    // get user details from frontend
+    // validation - not empty
+    // check if user if already exist;username,email
+    // check for image, check for avatar
+    // upload them to cloudnary, avatar
+    // create user object - create entry in db
+    //remove pasword and refresh token feild from response
+    // check for user creation 
+    //return response
+
 })
 
 export {
